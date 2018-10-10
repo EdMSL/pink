bba379f6b21aab89bf3937ec21d627c183a95cca
EdMSL <eduard2011rus@gmail.com>
2018-10-10 17:51:27 +0700

    Начата работа над адаптивностью

--- source/js/main.js
+++ source/js/main.js
@@ -2,16 +2,21 @@ var menuButton = document.querySelector('.main-nav__toggle');
 var menu = document.querySelector('.main-nav');
 var headerLogo = document.querySelector('.page-header__logo');
 
+menu.classList.remove('main-nav--no-js');
+menu.classList.remove('main-nav--open');
+menu.classList.add('main-nav--close');
+headerLogo.classList.remove('page-header__logo--fill');
+
 menuButton.addEventListener('click', function(evt) {
   evt.preventDefault();
-  if (menu.classList.contains('main-nav--close')) {
+  if (menu.classList.contains('main-nav--open')) {
+    menu.classList.remove('main-nav--open');
+    menu.classList.add('main-nav--close');
+    headerLogo.classList.remove('page-header__logo--fill');
+  }
+  else {
     menu.classList.remove('main-nav--close');
     menu.classList.add('main-nav--open');
     headerLogo.classList.add('page-header__logo--fill');
-  }
-  else {
-    menu.classList.add('main-nav--close');
-    menu.classList.remove('main-nav--open');
-    headerLogo.classList.remove('page-header__logo--fill');
   }
 })
