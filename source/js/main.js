1391fbb76eb61aad27e52072fcaa748132828a51
EdMSL <eduard2011rus@gmail.com>
2018-10-27 18:17:46 +0700

    Добавлены изображенния в WebP

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,6 @@
 var menuButton = document.querySelector('.main-nav__toggle');
 var menu = document.querySelector('.main-nav');
 var headerLogo = document.querySelector('.page-header__logo');
-var submitBtn = document.querySelector('.form-list__submit-btn');
-var surnameInput = document.querySelector('#form_surname');
-var nameInput = document.querySelector('#form_name');
-var emailInput = document.querySelector('#form_contacts_email');
-var popupSucces = document.querySelector('.pop-up--succes');
-var popupFailure = document.querySelector('.pop-up--failure');
-var popupSuccesBtn = popupSucces.querySelector('.pop-up__button');
-var popupFailureBtn = popupFailure.querySelector('.pop-up__button');
-// var storage = '';
-
-// try {
-// 	storage = localStorage.getItem('email');
-// } catch (err) {
-// 	isStorageSupport = false;
-// };
 
 menu.classList.remove('main-nav--no-js');
 menu.classList.remove('main-nav--open');
@@ -33,29 +18,5 @@ menuButton.addEventListener('click', function(evt) {
     menu.classList.remove('main-nav--close');
     menu.classList.add('main-nav--open');
     headerLogo.classList.add('page-header__logo--fill');
-  }
-});
-
-submitBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  if (!surnameInput.value || !nameInput.value || !emailInput.value) {
-    popupFailure.classList.add('pop-up--active');
-  }
-  else {
-    popupSucces.classList.add('pop-up--active');
-  }
-});
-
-popupFailureBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  if (popupFailure.classList.contains('pop-up--active')) {
-    popupFailure.classList.remove('pop-up--active');
-  }
-});
-
-popupSuccesBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  if (popupSucces.classList.contains('pop-up--active')) {
-    popupSucces.classList.remove('pop-up--active');
   }
 });
