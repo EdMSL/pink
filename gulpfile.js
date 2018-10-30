43d0e9583adc2def9573a6cc753eaf68ef2d5ba7
EdMSL <eduard2011rus@gmail.com>
2018-10-30 18:32:52 +0700

    Продолжаем работу над автоматизацией

--- gulpfile.js
+++ gulpfile.js
@@ -64,11 +64,16 @@ gulp.task("copy", function () {
 
 gulp.task("clean", function () {
   return del("build");
-})
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -76,9 +81,10 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh")); 
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("start", gulp.series("build", "server"));
 
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
